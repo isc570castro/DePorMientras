eb50a648a04df7c422d11c81bf5176995bdd3c5e
Marcos Flores Saucedo <marcos.flores.smart.lines@gmail.com>
2018-02-06 14:05:44 -0600

    Modificacion de menu

--- view/index.php
+++ view/index.php
@@ -53,34 +53,35 @@
                 </ul>
               </li>
               
+              <li class="admin4"><a href="?c=productos"><span class="glyphicon glyphicon-inbox"></span> Productos</a></li>
+              <li class="admin4"><a href="?c=deal"><span class="glyphicon glyphicon-list-alt"></span> Producción</a></li>
+
               <li class="dropdown" id="admin3">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-stats"></span> Estadisticas <span class="caret"></span></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                  <span class="glyphicon glyphicon-stats"></span> Estadisticas <span class="caret"></span>
+                </a>
                 <ul class="dropdown-menu">
                   <li><a href=""><span class="glyphicon glyphicon-tasks"></span> Panel</a></li>
                   <li><a href="?c=informe"><span class="glyphicon glyphicon-list"></span> Informe</a></li>
                 </ul>
               </li>
-
-
-              <li class="admin4"><a href="?c=productos"><span class="glyphicon glyphicon-inbox"></span> Productos</a></li>
-              <li class="admin4"><a href="?c=deal"><span class="glyphicon glyphicon-cog"></span> Producción</a></li>
             </ul>
-            <form class="navbar-form navbar-left" id="admin5">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Buscar">
-              </div>
-            </form>
+
             <ul class="nav navbar-nav navbar-right" id="admin7">
               <li><a href="#"><span class="glyphicon glyphicon-question-sign"></span></a></li>
               <li><a href="#"><span class="glyphicon glyphicon-bell"></span></a></li>
               <li>
-                <div class="user_admin dropdown" id="admin8"> <a href="javascript:void(0);" data-toggle="dropdown"><img src="assets/imagenes/user.png" /><span class="user_adminname"> John Doe</span> <b class="caret"></b> </a>
+                <div class="user_admin dropdown" id="admin8"> 
+                  <a href="javascript:void(0);" data-toggle="dropdown">
+                    <img src="assets/imagenes/user.png" />
+                    <span class="user_adminname"> John Doe</span> 
+                    <b class="caret"></b> 
+                  </a>
                   <ul class="dropdown-menu">
                     <div class="top_pointer"></div>
-                    <li> <a href="profile.html"><i class="fa fa-user"></i><span class="glyphicon glyphicon-user"></span> Perfil de Usuario</a> </li>
-                    <li> <a href="help.html"><i class="fa fa-question-circle"></i><span class="glyphicon glyphicon-cog"></span> Ajustes</a> </li>
-                    <li role="separator" class="divider"></li>
-                    <li> <a href="settings.html"><i class="fa fa-cog"></i> Setting </a></li>
+                    <li> <a href="profile.html"><i class="fa fa-user"></i><span class="glyphicon glyphicon-user">
+                      </span> Perfil de Usuario</a>
+                    </li>
                     <li role="separator" class="divider"></li>
                     <li> <a href="login.html"><i class="fa fa-power-off"></i><span class="glyphicon glyphicon-log-out"></span> Cerrar Sesión</a> </li>
                   </ul>
