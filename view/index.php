47b00a619afe3977052cd54e866387ca92312724
Marcos Flores Saucedo <marcos.flores.smart.lines@gmail.com>
2018-02-06 14:16:37 -0600

    agregué configuacion al menu

--- view/index.php
+++ view/index.php
@@ -65,6 +65,7 @@
                   <li><a href="?c=informe"><span class="glyphicon glyphicon-list"></span> Informe</a></li>
                 </ul>
               </li>
+              <li class="admin4"><a href="?c=administracion"><span class="glyphicon glyphicon-cog"></span> Configuración</a></li>
             </ul>
 
             <ul class="nav navbar-nav navbar-right" id="admin7">
