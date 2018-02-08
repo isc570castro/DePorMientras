a5c1f88f4391f27bc50b36528043e64515edc6ef
Marcos Flores Saucedo <marcos.flores.smart.lines@gmail.com>
2018-02-08 10:55:26 -0600

    creacion de archivos de vista configuracion

---
+++ view/administracion/embudos.php
@@ -0,0 +1,58 @@
+<div class="containerperso">
+  <div class="col-xs-8 col-sm-9">
+    <div class="row">
+      <div class="col-xs-10 col-sm-9 col-lg-9">
+        <h3>Personalizar etapas de venta</h3>
+      </div>
+      <br>
+      <div class="col-xs-2 col-lg-3" align="right">
+        <a href="" class="btn btn-success btn-xs">Añadir embudo nuevo</a>
+      </div> 
+    </div>
+  </div>
+</div>
+<div class="margen">
+  <div class="row">
+    <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+      <br>
+      <ul class="nav nav-tabs">
+        <li role="presentation" class="active"><a data-toggle="tab" href="#home">Home <span class="badge">10</span></a></li>
+        <li role="presentation"><a data-toggle="tab" href="#menu1">Menu1</a></li>
+      </ul>
+      <div class="tab-content margen" id="tbl">
+        <div id="home" class="tab-pane fade in active">
+          <div class="row">
+            <div class="col-xs-12 col-lg-2">
+              <h3>Pipeline</h3>
+            </div>
+            <div class="col-xs-7 col-lg-4">
+              <br>
+              <div class="btn-group">
+                <a href="#" class="btn btn-default btn-xs">Editar</a>
+              </div>
+              <div class="btn-group">
+                <a href="#" class="btn btn-default btn-xs">Eliminar embudo</a>
+              </div>
+            </div>
+            <div class="col-xs-5 col-lg-6" align="right">
+              <br>
+              <a href="#" class="btn btn-success btn-xs">Añadir etapa</a>
+            </div> 
+          </div>
+          <br>
+          <div class="row">
+            <div class="col-xs-12 col-lg-12">
+              <div class="jumbotron">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="menu1" class="tab-pane fade">
+          <h3>Menu 1</h3>
+          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
